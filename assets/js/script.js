a7c883f57e46fa57cd0f0c3be9d784b86df4be75
pilar garcia <pilargarciaroyo@gmail.com>
2023-08-08 12:43:47 -0400

    backgroun de breadcrumbs y main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// const logoContainer = document.querySelector(".logo-container");
+// const customTooltip = document.querySelector(".custom-tooltip");
+
+// logoContainer.addEventListener("mouseover", function () {
+//   customTooltip.style.display = "block"; // Mostrar el tooltip al pasar el cursor sobre el div
+// });
+
+// logoContainer.addEventListener("mouseout", function () {
+//   customTooltip.style.display = "none"; // Ocultar el tooltip al quitar el cursor del div
+// });
 function obtenerFechaActual() {
   const dias = [
     "domingo",
@@ -22,13 +32,11 @@ function obtenerFechaActual() {
     "noviembre",
     "diciembre",
   ];
-
   const fecha = new Date();
   const diaSemana = dias[fecha.getDay()];
   const dia = fecha.getDate();
   const mes = meses[fecha.getMonth()];
   const anio = fecha.getFullYear();
-
   return `${diaSemana}, ${dia} de ${mes} de ${anio}`;
 }
 function actualizarFechaActual() {
@@ -37,54 +45,27 @@ function actualizarFechaActual() {
 }
 actualizarFechaActual();
 setInterval(actualizarFechaActual, 1000);
+// FORMULARIO
+const form = document.getElementById("contact-form");
+const correoInput = document.getElementById("correo");
 
-const form = document.getElementById('contact-form');
-const correoInput = document.getElementById('correo');
-
-form.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
   if (!form.checkValidity()) {
-    event.preventDefault(); 
-    alert('Por favor, llene todos los campos correctamente.');
-}
-if (!form.checkValidity()) {
-  event.preventDefault(); 
-  alert('Por favor, llene todos los campos correctamente.');
-} else {
-    const correoValue = correoInput.value;
-    const correoRegex = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{1,3}$/; // Expresión regular para validar el correo
-    if (!correoRegex.test(correoValue)) {
-        event.preventDefault(); 
-        alert('Por favor, ingrese un correo válido: con @ y extensión final después del punto.');
-    }
-}
-});
-
-  document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Previene el envío del formulario por defecto
-
-    // Validar el formulario
-    if (validarFormulario()) {
-      // Si todas las validaciones son exitosas, muestra el alert
-      alert('El correo fue enviado exitosamente');
-      // Aquí puedes agregar el código para enviar el formulario al servidor si es necesario
-    }
-  });
-
-  function validarFormulario() {
-    // Aquí puedes agregar tus validaciones personalizadas para cada campo del formulario
-    // Retorna true si todas las validaciones son exitosas, o false si alguna falla
-
-    // Por ejemplo, puedes validar el correo usando la expresión regular que mencionamos anteriormente
-    const correoInput = document.getElementById('correo');
-    const correo = correoInput.value;
-    const regexCorreo = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{1,3}$/;
-
-    if (!regexCorreo.test(correo)) {
-      alert('Por favor, asegura que tus datos sean válidos');
-      return false;
-    }
-
-    // Puedes agregar más validaciones aquí si es necesario
-
-    return true; // Si todas las validaciones son exitosas, retorna true
+    alert("Por favor, llene todos los campos correctamente.");
+  } else if (!validarCorreo(correoInput.value)) {
+    alert(
+      "Por favor, ingrese un correo válido: con @ y extensión final después del punto."
+    );
+  } else {
+    alert("El correo fue enviado exitosamente");
+    limpiarFormulario(); 
   }
+});
+function validarCorreo(correo) {
+  const regexCorreo = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{1,3}$/;
+  return regexCorreo.test(correo);
+}
+function limpiarFormulario() {
+  form.reset(); 
+}
